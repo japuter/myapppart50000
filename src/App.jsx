cfbe0513e2a4bfc8c0b75915e59ddf9c6f180151
Japuter <jasper.kaira@gmail.com>
2024-04-22 18:10:44 +0300

    routin

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Home from './views/Home';
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import {Profile} from './views/Profile';
+import Upload from './views/Upload';
+import Layout from './views/Layout';
+import Single from './views/Single';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/upload" element={<Upload />} />
+          <Route path="/media/:id" element={<Single />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ src/views/Layout.jsx
@@ -0,0 +1,19 @@
+import {Link, Outlet} from 'react-router-dom';
+
+const Layout = () => (
+  <div>
+    <header>
+      <nav>
+        <Link to="/">Etusivu 👁👁👁</Link>
+        <Link to="/profile">Profiili 🐽</Link>
+        <Link to="/upload">Upload</Link>
+      </nav>
+    </header>
+    <main>
+      <Outlet />
+    </main>
+    <footer className="m-12 text-xl">Copyright 2024</footer>
+  </div>
+);
+
+export default Layout;
